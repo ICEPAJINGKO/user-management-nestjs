bdb222b629c4879715ad2317fe8dcfc1825b67ed
ICEPAJINGKO <icepajingko@gmail.com>
2023-06-18 23:54:22 +0700

    add config module from @nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: `.env.${process.env.NODE_ENV || 'development'}` }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+
+  await app.listen(configService.get('PORT'), () => {
+    console.info('\x1b[33m', `########## The service is currently running in the ${configService.get('NODE_ENV')} environment. ##########`, '\x1b[0m');
+    console.info('\x1b[33m', `########## Service is running on port ${configService.get('PORT')}. ##########`, '\x1b[0m');
+  });
 }
 bootstrap();
