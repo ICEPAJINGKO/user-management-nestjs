69f654662535bb1d0924e22c99d5dfa5e61a10f5
ICEPAJINGKO <icepajingko@gmail.com>
2023-06-19 21:15:02 +0700

    feat connect mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: `.env.${process.env.NODE_ENV || 'development'}` }),
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/user_management'),
     UsersModule,
   ],
   controllers: [AppController],
