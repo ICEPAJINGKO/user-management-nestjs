59fa0738f0025e649ddedb855f6a3a3f71f2c254
ICEPAJINGKO <icepajingko@gmail.com>
2023-06-26 02:55:43 +0700

    add constant and mock data files

---
+++ src/shared/constants.ts
@@ -0,0 +1,9 @@
+export const PREFIX = {
+    MR: 'Mr.',
+    MS: 'Ms.',
+    MRS: 'Mrs.',
+    MISS: 'Miss',
+    K: 'K.'
+};
+
+export const DEFAULT_PREFIX = PREFIX.K || 'K.';
---
+++ src/shared/mock-data.ts
@@ -0,0 +1,6 @@
+export const user = { id: 1, name: 'John Doe', email: 'john.doe@example.com' };
+
+export const users = [
+    { id: 1, name: 'John Doe', email: 'john.doe@example.com' },
+    { id: 2, name: 'Jane Smith', email: 'jane.smith@example.com' },
+];
